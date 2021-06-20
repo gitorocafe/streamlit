287c3ebe0983eeb98adb81fd3235c024923f6bd9
gitorocafe <lfa.gsx1300r@gmail.com>
2021-06-20 12:17:28 +0900

    todays commit

--- main.py
+++ main.py
@@ -8,24 +8,49 @@ st.title('RC梁検討')
 '''
 
 
-if st.checkbox('Show Image'):
-    st.write('Hello Streamlit2')
+# if st.checkbox('Show Image'):
+#     st.write('Hello Streamlit2')
 
-#option = st.selectbox('あなたの好きな数字は', list(range(1,11)))
+
+
+
+
+units_dic = {
+    'mm' : 'mm',
+    'cm' : 'cm',
+    'm' : 'm',
+    'N/mm2' : 'N/mm2',
+    '本':'本'
+}
+
+
+st.write('インタラクティブ')
+width = st.text_input('梁巾 B：' + units_dic['mm'])
+depth = st.text_input('梁せい D：' + units_dic['mm'])
+Fc = st.text_input('コンクリートの設計基準強度 Fc：' + units_dic['N/mm2'])
+
+bar_Dia = (
+    'D10',
+    'D13',
+    'D16',
+    'D19',
+    'D22',
+    'D25',
+    'D29'
+)
+
+option = st.selectbox('鉄筋径：', list(bar_Dia))
+nos = st.text_input('1段目鉄筋本数：' + units_dic['本'])
 
 #st.write('あなたの好きな数字は',option,'です')
 
-st.write('インタラクティブ')
-option = st.text_input('あなたの趣味を教えてください。')
-option,'です'
+# st.write('プログレスバーの表示')
+# 'start!!'
 
-st.write('プログレスバーの表示')
-'start!!'
+# latest_iteration = st.empty()
+# bar = st.progress(0)
 
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-    bar.progress(i+1)
-    latest_iteration.text(i+1)
-    time.sleep(0.05)
+# for i in range(100):
+#     bar.progress(i+1)
+#     latest_iteration.text(i+1)
+#     time.sleep(0.05)
